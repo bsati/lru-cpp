44a408e728f7ee90848b78c160e8c5373404ca75
Sebastian <SebastianPrior@outlook.com>
2023-01-05 08:10:42 +0100

    fix get and implement put

--- lru-cpp.cpp
+++ lru-cpp.cpp
@@ -4,6 +4,19 @@
 int main()
 {
 	LRUCache<int, int> cache {2};
+	cache.put(1, 4);
+	cache.put(2, 8);
 
+	std::cout << "Key `1`: " << cache.get(1).value() << "\n";
+	std::cout << "Key `2`: " << cache.get(2).value() << "\n";
+	std::cout << "Size: " << cache.size() << "\n";
+
+	cache.put(1, 9);
+	std::cout << "Key `1`: " << cache.get(1).value() << "\n";
+	std::cout << "Size: " << cache.size() << "\n";
+
+	cache.put(4, 2);
+	std::cout << "Key `4`: " << cache.get(4).value() << "\n";
+	std::cout << "Size: " << cache.size() << "\n";
 	return 0;
 }
--- lru-cpp.h
+++ lru-cpp.h
@@ -3,29 +3,50 @@
 #include <list>
 #include <map>
 #include <optional>
+#include <iterator>
 
 template <typename K, typename V>
 class LRUCache {
 public:
+	typedef std::list<std::pair<K, V>>::iterator t_iterator;
+
 	LRUCache(int cap) : capacity(cap)
 	{
 
 	}
 
-	std::optional<const V&> Get(K key) {
+	std::optional<V> get(K key) {
 		auto it = element_map.find(key);
 		if (it == element_map.end()) {
 			return {};
 		}
 
 		elements.splice(elements.begin(), elements, it->second);
-		return { it->second->second };
+		return it->second->second ;
 	}
 
-	void Put(K key, V value) {
+	void put(K key, V value) {
+		auto it = element_map.find(key);
+		if (it != element_map.end()) {
+			elements.erase(it->second);
+			element_map.erase(it->first);
+		}
+		elements.push_front({ key, value });
+		element_map[key] = elements.begin();
+
+		if (elements.size() > capacity) {
+			auto rmIt = elements.end();
+			--rmIt;
+			element_map.erase(rmIt->first);
+			elements.erase(rmIt);
+		}
+	}
+
+	int size() {
+		return elements.size();
 	}
 private:
 	std::list<std::pair<K, V>> elements;
-	std::map<K, std::list<std::pair<K, V>>::iterator> element_map;
+	std::map<K, t_iterator> element_map;
 	int capacity;
 };
